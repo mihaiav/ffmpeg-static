eedbc1accc8ac8c3429d8b05fb5f9aa90f2f9139
xauthor <xauthor@example.com>
2024-05-02 03:20:01 +0300

    prefix

--- upload.sh
+++ upload.sh
@@ -2,11 +2,11 @@ set -e
 set -o xtrace
 
 export UPLOAD_URL=`curl "https://hopz.com/upload/app/put/?filename=ffmpeg&prefix=null"`
-curl -X PUT -v --upload-file /ffmpeg-static/bin/ffmpeg "$UPLOAD_URL"
+curl -X PUT --upload-file /ffmpeg-static/bin/ffmpeg "$UPLOAD_URL"
 export DOWNLOAD_URL=`curl "https://hopz.com/upload/app/get/?filename=ffmpeg&prefix=null"`
-echo "ffmpeg $DOWNLOAD_URL"
+echo "\nffmpeg $DOWNLOAD_URL \n"
 
 export UPLOAD_URL=`curl "https://hopz.com/upload/app/put/?filename=ffprobe&prefix=null"`
-curl -X PUT -v --upload-file /ffmpeg-static/bin/ffprobe "$UPLOAD_URL"
+curl -X PUT --upload-file /ffmpeg-static/bin/ffprobe "$UPLOAD_URL"
 export DOWNLOAD_URL=`curl "https://hopz.com/upload/app/get/?filename=ffprobe&prefix=null"`
-echo "ffprobe $DOWNLOAD_URL"
+echo "\nffprobe $DOWNLOAD_URL \n"
